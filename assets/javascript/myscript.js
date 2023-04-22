b7d5019891459ab52ad73d112070e571bb022605
hantran <s3963227@rmit.edu.vn>
2023-04-23 02:51:32 +0700

    fantasy small update. reorganize css files

--- assets/javascript/myscript.js
+++ assets/javascript/myscript.js
@@ -9,18 +9,19 @@ Acknowledgement:
 - About us video: https://www.youtube.com/watch?v=AcfM9Z9MbXI
 - Icon: https://fontawesome.com/ */
 
-/*activate header on scroll */
+//activate header on scroll
 const header = document.querySelector("header");
 window.addEventListener("scroll", function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 })
 
+// Hide logo on scroll
 const logo = document.querySelector(".header-logo")
 window.addEventListener("scroll", function(){
     logo.classList.toggle("logo-remove", window.scrollY > 0);
 })
 
-/*Sidebar nav toggle*/
+//Sidebar nav toggle
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 menu.onclick = () => {
